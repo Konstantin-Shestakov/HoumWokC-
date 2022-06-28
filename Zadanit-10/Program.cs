b0e6db20e632fc1ae6811b346a34d2816648d239
Konstantin <shestakov_k@mail.ru>
2022-06-28 19:21:34 +0300

    add zadanie 10

---
+++ Zadanit-10/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Input namber:  ");
+int namber = Convert.ToInt32(Console.ReadLine());
+if (namber <= 999 & namber >= 100) // int FirstInt = (namber / 100);Console.WriteLine(" Первая цbфра  " + FirstInt);
+{
+  int SecondInt = (namber / 10);
+  SecondInt = SecondInt % 10;
+  Console.WriteLine(namber + "  ->  " + SecondInt); // int ThirdInt = (namber % 10);  Console.WriteLine(" Третья цифра  " + ThirdInt);
+ 
+ }
+else
+{
+    Console.WriteLine("Введите трехзначное число");
+}
