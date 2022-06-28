51b56b1ef7188821e126b9f27cf10ec4600f16a0
Konstantin <shestakov_k@mail.ru>
2022-06-28 21:32:25 +0300

    add zadanie 15

--- Zadanit-10/Program.cs
+++ Zadanit-10/Program.cs
@@ -18,7 +18,7 @@ else
     Console.WriteLine("Введите трехзначное число");
 }
 */
-//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+/* //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
 //645 -> 5
 
@@ -40,6 +40,34 @@ while (CorNamber >= 1000)
 int ThirdInt = (CorNamber % 10);  
     Console.WriteLine( Namber + " -> " + ThirdInt);
 }
-
-
-
+*/
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+        //6 -> да
+        //7 -> да
+        //1 -> нет
+        Console.WriteLine("Input namber:  ");
+        int Namber = Convert.ToInt32(Console.ReadLine());
+        if (Namber >= 1 & Namber <= 7)
+        {
+            if (Namber >= 1 & Namber <= 5)
+            {
+                Console.WriteLine(Namber + " ->  нет");
+            }
+            else
+            {
+                if (Namber == 6 || Namber == 7)
+                {
+                    Console.WriteLine(Namber + " ->  да ");
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Enter a number from 1... to 7");
+        }
+    }
+}
